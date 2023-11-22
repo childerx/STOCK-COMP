6bba18c9e4942c00811aeee94d06d6c8d1f43d17
ibrahzizo360 <iaziz@st.ug.edu.gh>
2023-11-22 00:55:07 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex-1'>
-
+    <main className=''>
+      <h1 className='text-5xl mx-auto text-center'>Hello World</h1>
     </main>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <nav className='h-screen hidden lg:block lg:flex-shrink-0 lg:bg-gray-800 lg:overflow-y-auto'>Sidebar</nav>
+    <nav className='h-screen hidden lg:block lg:flex-shrink-0 lg:bg-gray-800 lg:overflow-y-auto'>
+        <div className="relative w-60 flex space-y-16 flex-col p-3">
+            <h1>hello</h1>
+        </div>
+    </nav>
   )
 }
 
