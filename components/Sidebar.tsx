c3dda0f1ad0809c3e33000ff3b0bb668210d938c
ibrahzizo360 <iaziz@st.ug.edu.gh>
2023-11-22 00:47:48 +0000

    .

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <nav className='h-screen hidden lg:block lg:flex-shrink-0 lg:bg-gray-800 lg:overflow-y-auto'>Sidebar</nav>
+  )
+}
+
+export default Sidebar
