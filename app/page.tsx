150e3bd76c6e45d90aba90f6e59180ec284f8465
Nana Kwame <iabu007@st.ug.edu.gh>
2023-11-22 02:29:23 +0000

    .home

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,201 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-10">
-      <div className="flex items-center">
-        <div className="">
+    <main className="mt-16 p-16 border-t-[1px]">
+      <header className="flex items-center gap-5">
+        <div className="w-[100%]">
           <h1 className="text-[48px] text-[#1E1E1E] font-[600]">
             The <span className="text-[#0400DD]">stock market </span> is a
             device for transferring money from the
-            <span className="text-[#0400DD]">impatient</span> to the{" "}
+            <span className="text-[#0400DD]"> impatient</span> to the{" "}
             <span className="text-[#0400DD]">patient.</span>
           </h1>
-          <span className="text-[#6F6F6F] text-[20px] font-[500]">
+          <span className="text-[#6F6F6F] text-[20px] font-[500] uppercase mt-6 block ">
             warren buffet
           </span>
         </div>
 
-        <div className="">
-          {/* <Image src='/images/warren.jpg' alt='home page image' width={300} height={300} /> */}
+        <img
+          src="/images/homeProfile.svg"
+          alt="home page image"
+          width={"100%"}
+          height={"450px"}
+          className="h-[450px] object-cover"
+        />
+      </header>
+
+      <section className="text-center mt-16 px-10 mx-auto">
+        <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
+          {" "}
+          overview of the stock market
+        </h1>
+        <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus non
+          enim praesent elementum facilisis leo vel fringilla est. Tristique
+          senectus et netus et malesuada fames. Amet mattis vulputate enim nulla
+          aliquet. Malesuada bibendum arcu vitae elementum curabitur. Ultrices
+          sagittis orci a scelerisque purus semper eget duis. Sodales ut eu sem
+          integer vitae justo eget magna. Nisl purus in mollis nunc sed id
+          semper risus. Sed vulputate odio ut enim blandit volutpat maecenas.
+          Gravida dictum fusce ut placerat orci nulla pellentesque dignissim
+          enim. Est ultricies integer quis auctor elit sed vulputate mi. Euismod
+          nisi porta lorem mollis aliquam ut. Quis vel eros donec ac odio tempor
+          orci dapibus ultrices.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Fusce id
+          velit ut tortor pretium viverra. Dignissim sodales ut eu sem integer
+          vitae justo eget. Et ligula ullamcorper malesuada proin libero nunc.
+          Ultrices in iaculis nunc sed augue lacus. Sed lectus vestibulum mattis
+          ullamcorper velit sed ullamcorper morbi. Tempus quam pellentesque nec
+          nam aliquam. Risus quis varius quam quisque. Ultricies mi eget mauris
+          pharetra. Ultrices mi tempus imperdiet nulla malesuada pellentesque
+          elit. Porttitor eget dolor morbi non arcu risus.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8">
+          Enim blandit volutpat maecenas volutpat blandit aliquam. Facilisi
+          etiam dignissim diam quis enim lobortis scelerisque fermentum dui.
+          Aliquet enim tortor at auctor. Elit pellentesque habitant morbi
+          tristique senectus et. A condimentum vitae sapien pellentesque
+          habitant. Amet facilisis magna etiam tempor orci eu lobortis elementum
+          nibh. Volutpat diam ut venenatis tellus in metus vulputate. Sagittis
+          orci a scelerisque purus semper. At tempor commodo ullamcorper a. Non
+          diam phasellus vestibulum lorem sed risus ultricies. Ullamcorper morbi
+          tincidunt ornare massa eget egestas purus viverra.
+        </p>
+      </section>
+      <section className="text-center mt-16 px-10 mx-auto">
+        <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
+          {" "}
+          overview of ghana stock exchange
+        </h1>
+        <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus non
+          enim praesent elementum facilisis leo vel fringilla est. Tristique
+          senectus et netus et malesuada fames. Amet mattis vulputate enim nulla
+          aliquet. Malesuada bibendum arcu vitae elementum curabitur. Ultrices
+          sagittis orci a scelerisque purus semper eget duis. Sodales ut eu sem
+          integer vitae justo eget magna. Nisl purus in mollis nunc sed id
+          semper risus. Sed vulputate odio ut enim blandit volutpat maecenas.
+          Gravida dictum fusce ut placerat orci nulla pellentesque dignissim
+          enim. Est ultricies integer quis auctor elit sed vulputate mi. Euismod
+          nisi porta lorem mollis aliquam ut. Quis vel eros donec ac odio tempor
+          orci dapibus ultrices.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Fusce id
+          velit ut tortor pretium viverra. Dignissim sodales ut eu sem integer
+          vitae justo eget. Et ligula ullamcorper malesuada proin libero nunc.
+          Ultrices in iaculis nunc sed augue lacus. Sed lectus vestibulum mattis
+          ullamcorper velit sed ullamcorper morbi. Tempus quam pellentesque nec
+          nam aliquam. Risus quis varius quam quisque. Ultricies mi eget mauris
+          pharetra. Ultrices mi tempus imperdiet nulla malesuada pellentesque
+          elit. Porttitor eget dolor morbi non arcu risus.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8">
+          Enim blandit volutpat maecenas volutpat blandit aliquam. Facilisi
+          etiam dignissim diam quis enim lobortis scelerisque fermentum dui.
+          Aliquet enim tortor at auctor. Elit pellentesque habitant morbi
+          tristique senectus et. A condimentum vitae sapien pellentesque
+          habitant. Amet facilisis magna etiam tempor orci eu lobortis elementum
+          nibh. Volutpat diam ut venenatis tellus in metus vulputate. Sagittis
+          orci a scelerisque purus semper. At tempor commodo ullamcorper a. Non
+          diam phasellus vestibulum lorem sed risus ultricies. Ullamcorper morbi
+          tincidunt ornare massa eget egestas purus viverra.
+        </p>
+      </section>
+
+      <section className="text-center mt-16 px-10 mx-auto">
+        <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
+          {" "}
+          companies of the ghana stock exchange
+        </h1>
+        <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
+
+        <div className="flex items-center justify-around px-12">
+          <img src="images/mtn.svg" alt="mtn" width={"60px"} height={"60px"} />
+          <img
+            src="images/ecobank.svg"
+            alt="ecobank"
+            width={"110px"}
+            height={"60px"}
+          />
+          <img
+            src="images/calbank.svg"
+            alt="calbank"
+            width={"120px"}
+            height={"60px"}
+          />
+          <img
+            src="images/goil.svg"
+            alt="goil"
+            width={"60px"}
+            height={"60px"}
+          />
+          <img
+            src="images/guinnes.svg"
+            alt="guinnes"
+            width={"60px"}
+            height={"60px"}
+          />
         </div>
-      </div>
+      </section>
+
+      <section className="text-center mt-16 px-10 mx-auto">
+        <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
+          {" "}
+          mtn ghana
+        </h1>
+        <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus non
+          enim praesent elementum facilisis leo vel fringilla est. Tristique
+          senectus et netus et malesuada fames. Amet mattis vulputate enim nulla
+          aliquet. Malesuada bibendum arcu vitae elementum curabitur. Ultrices
+          sagittis orci a scelerisque purus semper eget duis. Sodales ut eu sem
+          integer vitae justo eget magna. Nisl purus in mollis nunc sed id
+          semper risus. Sed vulputate odio ut enim blandit volutpat maecenas.
+          Gravida dictum fusce ut placerat orci nulla pellentesque dignissim
+          enim. Est ultricies integer quis auctor elit sed vulputate mi. Euismod
+          nisi porta lorem mollis aliquam ut. Quis vel eros donec ac odio tempor
+          orci dapibus ultrices.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8 mb-6">
+          Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Fusce id
+          velit ut tortor pretium viverra. Dignissim sodales ut eu sem integer
+          vitae justo eget. Et ligula ullamcorper malesuada proin libero nunc.
+          Ultrices in iaculis nunc sed augue lacus. Sed lectus vestibulum mattis
+          ullamcorper velit sed ullamcorper morbi. Tempus quam pellentesque nec
+          nam aliquam. Risus quis varius quam quisque. Ultricies mi eget mauris
+          pharetra. Ultrices mi tempus imperdiet nulla malesuada pellentesque
+          elit. Porttitor eget dolor morbi non arcu risus.
+        </p>
+
+        <p className="text-[18px] font-[400] leading-8">
+          Enim blandit volutpat maecenas volutpat blandit aliquam. Facilisi
+          etiam dignissim diam quis enim lobortis scelerisque fermentum dui.
+          Aliquet enim tortor at auctor. Elit pellentesque habitant morbi
+          tristique senectus et. A condimentum vitae sapien pellentesque
+          habitant. Amet facilisis magna etiam tempor orci eu lobortis elementum
+          nibh. Volutpat diam ut venenatis tellus in metus vulputate. Sagittis
+          orci a scelerisque purus semper. At tempor commodo ullamcorper a. Non
+          diam phasellus vestibulum lorem sed risus ultricies. Ullamcorper morbi
+          tincidunt ornare massa eget egestas purus viverra.
+        </p>
+      </section>
     </main>
   );
 }
