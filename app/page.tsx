8db7019015b848bf74576ead7c7ce9176a40349b
Nana Kwame <iabu007@st.ug.edu.gh>
2023-11-22 01:14:49 +0000

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=''>
-      <h1 className='text-5xl mx-auto text-center'>Hello World</h1>
+    <main className="p-10">
+      <div className="flex items-center">
+        <div className="">
+          <h1 className="text-[48px] text-[#1E1E1E] font-[600]">
+            The <span className="text-[#0400DD]">stock market </span> is a
+            device for transferring money from the
+            <span className="text-[#0400DD]">impatient</span> to the{" "}
+            <span className="text-[#0400DD]">patient.</span>
+          </h1>
+          <span className="text-[#6F6F6F] text-[20px] font-[500]">
+            warren buffet
+          </span>
+        </div>
+
+        <div className="">
+          {/* <Image src='/images/warren.jpg' alt='home page image' width={300} height={300} /> */}
+        </div>
+      </div>
     </main>
-  )
+  );
 }
