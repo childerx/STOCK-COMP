8771fb9821c7d770ba2f3b2ac87f022af9594181
ibrahzizo360 <iaziz@st.ug.edu.gh>
2023-11-22 02:24:25 +0000

    .

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,13 +1,57 @@
+'use client'
+import { navigationLinks } from '@/constants';
 import Image from 'next/image'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
+type navLink = {
+    name: string;
+    path: string;
+    img_src: string;
+};
+
 const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
-    <nav className='h-screen hidden lg:block lg:flex-shrink-0 lg:bg-[#F5F5F5] lg:overflow-y-auto'>
-        <div className="relative w-60 flex flex-col p-4">
+    <nav className='h-screen hidden lg:block  lg:bg-[#F5F5F5] lg:overflow-y-auto'>
+        <div className="relative w-80 flex flex-col p-4">
             <div className='flex gap-4 p-2 justify-start items-center'>
                 <Image src='/images/Logo.svg' alt='logo' width={30} height={30} />
                 <h1 className='text-2xl text-center font-medium'>MS</h1>
+            </div>
+
+            <div className='flex flex-col gap-6 p-1 mt-4'>
+                {navigationLinks.map((navigationLink: navLink) => (
+                <Link
+                    key={navigationLink.path}
+                    href={navigationLink.path}
+                    className={`hover:text-blue-500 transition-colors duration-300 ${
+                    navigationLink.path === pathname ? 'text-active bg-selected' : 'text-normal'
+                    }`}
+                >
+                    <div className='flex gap-6 p-2'>
+                        <Image src={navigationLink.img_src} alt={navigationLink.name} width={20} height={20} />
+                        {navigationLink.name}
+                    </div>
+                </Link>
+                
+                ))}
+
+                <div className='flex gap-9 p-2 mt-48 flex-col'>
+                        <div className='flex gap-6 ml-2'>
+                            <Image src={"/images/chat.svg"} alt={"chat"} width={20} height={20} />
+                            <p>About MimTech Solutions Team</p>
+                        </div>
+                        <div className='flex gap-6'>
+                            <Image src={"/images/profile.svg"} alt={"profile"} width={40} height={40} />
+                            <div>
+                                <p className=''>Michael Smith</p>
+                                <p className='text-sm'>michaelsmith12@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     </nav>
@@ -15,3 +59,4 @@ const Sidebar = () => {
 }
 
 export default Sidebar
+
---
+++ constants/index.ts
@@ -0,0 +1,47 @@
+export const navigationLinks = [
+    {
+        name: 'Home',
+        path: '/',
+        img_src: '/images/home.svg',
+    },
+    {
+        name: 'Investment Thesis',
+        path: '/investment-thesis',
+        img_src: '/images/trend.svg',
+    },
+    {
+        name: 'Industrial Analysis',
+        path: '/industrial-analysis',
+        img_src: '/images/trend.svg',
+    },
+    {
+        name: 'Economic Analysis',
+        path: '/economic-analysis',
+        img_src: '/images/bill.svg',
+    },
+    {
+        name: 'Summary Data',
+        path: '/summary-data',
+        img_src: '/images/summary.svg',
+    }, 
+    {
+        name: 'Company Analysis',
+        path: '/company-analysis',
+        img_src: '/images/box.svg',
+    }, 
+    {
+        name: 'Valuation',
+        path: '/valuation',
+        img_src: '/images/box.svg',
+    }, 
+    {
+        name: 'Sensitive Analysis',
+        path: '/sensitive-analysis',
+        img_src: '/images/trend.svg',
+    }, 
+    {
+        name: 'Porter Five, SWOT, Pestle Analysis',
+        path: '/porter-five-swot-pestle-analysis',
+        img_src: '/images/trend.svg',
+    }
+]
