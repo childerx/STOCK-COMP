a346db01e0599e45c5e004aab4dc50acec7d8e74
AugustineLove <augustinelovestephens@gmail.com>
2023-11-22 15:07:22 +0000

    ...

--- app/sensitive-analysis/page.tsx
+++ app/sensitive-analysis/page.tsx
@@ -4,18 +4,13 @@ import React from 'react'
 const page = () => {
   return (
     <main className="mt-16 p-16 border-t border-t-[#e3e3e3] w-full text-center mx-auto">
-      <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
-        {" "}
-        SENSITIVITY ANALYSIS: Table
-      </h1>
+      <h1 className='text-center mt-10 text-2xl font-[300]'>SENSITIVITY ANALYSIS: <span className='font-[700]'>Table</span></h1>
       <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
       <div className='flex mt-30 justify-center items-center ml-16 mb-10'>
             <Image src={"/images/charts/table_1.png"} alt={"industrial-analysis"} width={1000} height={700} />
         </div> 
-        <h1 className="text-[#1E1E1E] font-[600] text-[24px] uppercase mb-2">
-        {" "}
-        SENSITIVITY ANALYSIS: Detailed Analysis
-      </h1>
+        
+        <h1 className='text-center mt-10 text-2xl font-[300]'>SENSITIVITY ANALYSIS: <span className='font-[700]'>Detailed Analysis</span></h1>
       <div className="h-1 bg-[#0F0CB0] rounded-full w-[25%] mx-auto mb-10" />
       <p className="text-[18px] font-[400] leading-8 mb-6">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
