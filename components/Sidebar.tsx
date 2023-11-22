d48d091eead0f49f9f913cb0ad9286685d3e6e8e
ibrahzizo360 <iaziz@st.ug.edu.gh>
2023-11-22 03:49:06 +0000

    .

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,8 +15,8 @@ const Sidebar = () => {
   const pathname = usePathname()
 
   return (
-    <nav className='h-screen hidden lg:block  lg:bg-[#F5F5F5] lg:overflow-y-auto'>
-        <div className="relative w-80 flex flex-col p-4">
+    <nav className='h-screen hidden lg:block  lg:bg-[#F5F5F5] lg:overflow-y-auto fixed'>
+        <div className=" w-80 flex flex-col p-4">
             <div className='flex gap-4 p-2 justify-start items-center'>
                 <Image src='/images/Logo.svg' alt='logo' width={30} height={30} />
                 <h1 className='text-2xl text-center font-medium'>MS</h1>
